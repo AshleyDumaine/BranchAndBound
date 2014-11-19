516487f8e8aa1f36c311623c4dac7ba183c9f01c
Ashley Dumaine <ashley.dumaine@uconn.edu>
2014-11-18 22:21:11 -0500

    removed a header file

--- knapsack_serial.c
+++ knapsack_serial.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+int compare (Node* arg0, Node* arg1) {
+  if(arg0->_ratio < arg1->_ratio)
+    return -1;
+  else if (arg0->_ration > arg1->_ratio)
+    return 1;
+  else
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
   if(argc != 2) {
@@ -6,7 +14,8 @@ int main(int argc, char* argv[]) {
     return 1;
   }
   char* filename = argv[1];
-  FILE fp = open(filename, 'r');
+  FILE* fp = fopen(filename, 'r');
+  //scan some  stuff
   
 }
 
--- priorityQueue.h
+++ priorityQueue.h
@@ -3,6 +3,7 @@
 
 typedef struct PQNode {
   struct PQNode* _next;
+  double _ratio;
   int _key;
   int _value;
   int _cap;
@@ -11,6 +12,11 @@ typedef struct PQNode {
   double _ub;
   struct LBound* _lb;
 } PQNode;
+
+typedef struct LBound {
+  pthread_rwlock_t _lock;
+  int _lb;
+} LBound;
 
 typedef struct priorityQueue {
   int _msx;
