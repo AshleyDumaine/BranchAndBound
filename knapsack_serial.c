e2e1df569f1ba8d2d64c68953a42abe293395c2c
Ashley Dumaine <ashley.dumaine@uconn.edu>
2014-11-20 14:10:57 -0500

    fixed bug

--- knapsack_serial.c
+++ knapsack_serial.c
@@ -205,7 +205,7 @@ void pathtranverse(PQueue* theQueue, PQNode* original){
 }
 
 int main(int argc, char* argv[]) {
-  if(argc != 2) {
+  if(argc != 3) {
     fprintf(stderr, "usage: knapsack_serial filename thread count\n");
     return 1;
   }
