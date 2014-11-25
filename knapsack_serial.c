6718bb668ff5cfd0ea4f23c83d0a684106870ee7
Ashley Dumaine <ashley.dumaine@uconn.edu>
2014-11-25 18:13:25 -0500

    still working on serial

--- knapsack_serial.c
+++ knapsack_serial.c
@@ -15,39 +15,21 @@ void pathtranverse(heap_t* theQueue, PQNode* original);
 int compare (const void* a, const void* b) {
   const Item** arg0  = (const Item**)a;
   const Item** arg1 = (const Item**)b;
-  if((*arg0)->_ratio < (*arg1)->_ratio)
-    return -1;
-  else if ((*arg0)->_ratio > (*arg1)->_ratio)
-    return 1;
-  else
-    return 0;
+  if((*arg0)->_ratio < (*arg1)->_ratio)return -1;
+  else if ((*arg0)->_ratio > (*arg1)->_ratio) return 1;
+  else return 0;
 }
 
 PQNode* deQueueWork(heap_t* twq)
 {
-  //pthread_mutex_lock(&twq->_lock);
-  //clock_t begin = clock();
-  while (isEmpty(twq)) {
-  //if(twq->_isDone == 0) 
-  //  pthread_cond_wait(&twq->_cond,&twq->_lock);
-  //else {
-      //pthread_mutex_unlock(&twq->_lock);
-      return NULL; 
-      //  }
-  }
+  if (isEmpty(twq))return NULL; 
   PQNode* n = deQueue(twq);
-  //clock_t end = clock();
-  //printf("time dequeue: %f \n", (double)(end-begin)/CLOCKS_PER_SEC);
-  //pthread_mutex_unlock(&twq->_lock);
   return n;
 }
 
 void enQueueWork(heap_t* twq,PQNode* t)
 {
-  //pthread_mutex_lock(&twq->_lock);
   enQueue(twq,t);
-  //pthread_cond_signal(&twq->_cond);
-  //pthread_mutex_unlock(&twq->_lock);
 }
 
 void calculateUpperBound(Item** itemArray,PQNode* node, int len) {
@@ -78,44 +60,23 @@ void calculateUpperBound(Item** itemArray,PQNode* node, int len) {
 void* bb(void* SQueue) {
   heap_t* theQueue = (heap_t*)SQueue;
   while(1){
-    //pthread_mutex_lock(&(theQueue->_lock));
-    //if (isEmpty(theQueue) && theQueue->_awakeThreads == 1)
-    //{
-    //	pthread_cond_broadcast(&(theQueue->_cond));
-    //	theQueue->_isDone = 1;
-    //}
-    //else
-    //theQueue->_awakeThreads--;
-    //pthread_mutex_unlock(&(theQueue->_lock));
     PQNode* original = deQueueWork(theQueue);
-    if (original==NULL)
-      return;
-    //pthread_mutex_lock(&(theQueue->_lock));
-    //theQueue->_awakeThreads++;
-    //pthread_mutex_unlock(&(theQueue->_lock));
+    if (original==NULL)return;
     Item** itemArray = theQueue->_itArrayptr;    
     if(original->_index == 0){
       if(original->_cap >= itemArray[0]->_weight)
 	original->_value = itemArray[0]->_profit + original->_value;
-      //pthread_rwlock_wrlock(&(lb->_lock));
       if(original->_value > lb->_lb)
 	lb->_lb = original->_value;
       free(original);
-      //pthread_rwlock_unlock(&(lb->_lock));
     }
-    
     else if(original->_cap == 0){
-      // pthread_rwlock_wrlock(&lb->_lock);
-      if(original->_value > lb->_lb)
-	lb->_lb= original->_value;
+      if(original->_value > lb->_lb)lb->_lb= original->_value;
       free(original);
-      // pthread_rwlock_unlock(&lb->_lock);
     }
-    else{
+    else
       pathtranverse(theQueue, original);
-    }
-    if(isEmpty(theQueue))
-      continue;
+    if(isEmpty(theQueue)) continue;
   }
 }
 
@@ -124,30 +85,19 @@ void pathtranverse(heap_t* theQueue, PQNode* original){
   while (original->_cap != 0 && original->_index != 0) {
     int index = original->_index;
     calculateUpperBound(itemArray,original,theQueue->_arraylength);
-    //pthread_rwlock_rdlock(&lb->_lock);
-    if(original->_ub < lb->_lb){
-      pthread_rwlock_unlock(&lb->_lock);
-      break;
-    }
-    //pthread_rwlock_unlock(&lb->_lock);
+    if(original->_ub < lb->_lb) break;
     original->_left = (PQNode*)malloc(sizeof(PQNode));
     original->_right = (PQNode*)malloc(sizeof(PQNode));
     PQNode* left = original->_left;
     PQNode* right = original->_right;
-    //pthread_rwlock_wrlock(&lb->_lock);
     right->_lb = lb;
     left->_lb = lb;
-    //pthread_rwlock_unlock(&lb->_lock);
     right->_index = (original->_index) - 1;
     right->_cap = original->_cap;
     right->_value = original->_value;
     calculateUpperBound(itemArray,right,theQueue->_arraylength);
-    //pthread_rwlock_rdlock(&lb->_lock);
-    if(right->_ub > lb->_lb)
-      enQueueWork(theQueue,right);
-    else
-      free(right);
-    //pthread_rwlock_unlock(&lb->_lock);
+    if(right->_ub > lb->_lb) enQueueWork(theQueue,right);
+    else free(right);
     left->_index = (original->_index) - 1;
     left->_cap =  original->_cap - itemArray[index]->_weight;
     left->_value = itemArray[index]->_profit + original->_value;
@@ -157,33 +107,25 @@ void pathtranverse(heap_t* theQueue, PQNode* original){
       break;
     }
     if(original->_cap < itemArray[index]->_weight){
-      //pthread_rwlock_wrlock(&lb->_lock);
       if(lb->_lb < original->_value)
 	lb->_lb = original->_value;
-      //pthread_rwlock_unlock(&(lb->_lock));
       break;
     }
-    //pthread_rwlock_rdlock(&lb->_lock);
     if(left->_ub < lb->_lb){
       free(left);
-      //pthread_rwlock_unlock(&lb->_lock);
-	  break;
+      break;
     }
-    //pthread_rwlock_unlock(&lb->_lock);
     PQNode* temp = original;
     free(temp);
     original = left;   
   }
-  //pthread_rwlock_rdlock(&lb->_lock);
   if(original->_value >= lb->_lb)
     lb->_lb= original->_value;
-  //pthread_rwlock_unlock(&lb->_lock);
   free(original);
   return;
 }
 
 int main(int argc, char* argv[]) {
-
   if(argc != 2) {
     fprintf(stderr, "usage: knapsack_serial filename\n");
     return 1;
@@ -220,9 +162,6 @@ int main(int argc, char* argv[]) {
   sharedQ->_arraylength = len;
   PQNode* startnode = (PQNode*)malloc(sizeof(PQNode));
   pthread_mutex_init(&mtx, NULL);
-  //pthread_mutex_lock(&sharedQ->_lock);
-  sharedQ->_awakeThreads = nthreads;
-  //pthread_mutex_unlock(&sharedQ->_lock);
   lb  = (LBound*)malloc(sizeof(LBound));
   pthread_rwlock_init(&lb->_lock, NULL);
   lb->_lb = 0;
@@ -241,9 +180,7 @@ int main(int argc, char* argv[]) {
   }
   for(i=0;i<nthreads;i++)
     pthread_join(threads[i],&exitStatus);
-  //pthread_rwlock_rdlock(&lb->_lock);
-  printf("lower bound is: %d \n",lb->_lb);
-  //pthread_rwlock_unlock(&lb->_lock);
+   printf("lower bound is: %d \n",lb->_lb);
   clock_t toc = clock();
   printf("Elapsed: %f seconds \n", (double)(toc-tic)/CLOCKS_PER_SEC);
   destroyQueue(sharedQ);
